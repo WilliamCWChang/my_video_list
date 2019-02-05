40794062c3390be9a756a00f62d16303a2408af1
William <iwtfagp@gmail.com>
2019-02-05 19:16:25 +0800

    add my list

--- check_my_process.py
+++ check_my_process.py
@@ -1,28 +1,57 @@
 import csv
+import requests
+from bs4 import BeautifulSoup
 
 
-def get_comic_list(filename):
+def get_comic_list_from_csv(csvfilename):
     comic_list = []
     with open(filename, newline='', encoding='utf8') as csvfile:
         for row in csv.reader(csvfile):
+            if len(row) < 3:
+                continue
             comic_list.append(
-                {"unread": row[0],
-                 "my_process": row[1],
-                 "now_process": row[2],
-                 "url": row[3],
-                 "title": row[4],
-                 })
+                {
+                    "my_process": row[1],
+                    "url": row[3],
+                })
     return comic_list
 
 
-import requests
-from bs4 import BeautifulSoup
-import re
+def set_comic_list_to_csv(filename, comic_list):
+    with open(filename, newline='', encoding='utf8', mode='w') as csvfile:
+        data_list = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for comic in comic_list:
+            data_list.writerow(comic)
+            print(comic)
 
 
-for comic in get_comic_list('my_anime1_list.csv'):
+filename = 'Readme.md'
+
+set_comic_list = []
+for comic in get_comic_list_from_csv(filename):
+
     r = requests.get(comic["url"])
     soup = BeautifulSoup(r.text, 'html.parser')
-    data = soup.main.h2.find_all('a')[0].text
 
-    print(comic["title"] + " now is " + data)
+    if "近期文章" in soup.main.h2.text:
+        print("The page is not found. url={}".format(comic["url"]))
+        continue
+
+    print(soup.main.h2)
+    last_video_title = soup.main.h2.find_all('a')[0].text
+    last_video_name = last_video_title.split("[")[0]
+    last_video_num = last_video_title.split("[")[1].split("]")[0]
+    unread = "v" if comic["my_process"] != last_video_num else ""
+
+    data = [
+        unread,
+        comic["my_process"],
+        last_video_num,
+        comic["url"],
+        last_video_name
+    ]
+    print(data)
+    set_comic_list.append(data)
+
+
+set_comic_list_to_csv(filename, set_comic_list)
